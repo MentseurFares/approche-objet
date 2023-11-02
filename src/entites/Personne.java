0e3564e11716d71d6adbbf2eab4fc51333c076ef
MentseurFares <mentseur.fares@hotmail.fr>
2023-11-02 12:12:02 +0100

    TP3 - Instances et méthodes

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -1,7 +1,5 @@
 package entites;
 
-import entites.AdressePostale;
-
 public class Personne {
     private String prenom;
     private String nom;
@@ -16,5 +14,33 @@ public class Personne {
         this.prenom = prenom;
         this.nom = nom;
         this.adressePostale = adressePostale;
+    }
+
+    public void affichagePersonneName() {
+        System.out.println("Prenom, Nom : " + this.prenom + " " + this.nom.toUpperCase());
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdressePostale(AdressePostale adressePostale) {
+        this.adressePostale = adressePostale;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public AdressePostale getAdressePostale() {
+        return adressePostale;
     }
 }
--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -2,9 +2,17 @@ package entites;
 
 public class TestPersonne {
     public static void main(String[] args) {
-        AdressePostale adresse1 = new AdressePostale(45,"Rue du Faubourg Saint Jaumes","3400","Montpellier");
-        AdressePostale adresse2 = new AdressePostale(64, "Avenue de Castelnau","34090","Montpellier");
-        Personne personne1 = new Personne("Fares","MENTSEUR",adresse1);
-        Personne personne2 = new Personne("Mehdi","LAMALI",adresse2);
+        AdressePostale adresse = new AdressePostale(45,"Rue du Faubourg Saint Jaumes","3400","Montpellier");
+        Personne personne = new Personne("Fares","MENTSEUR",adresse);
+        // affichage nom et prenom
+        personne.affichagePersonneName();
+        // modification des information de personne
+        personne.setPrenom("samy");
+        personne.setNom("mentseur");
+        adresse = new AdressePostale(23,"Rue de la liberté ", "31000","Toulouse");
+        personne.setAdressePostale(adresse);
+        //affichage nouvelles infos de personne
+        personne.affichagePersonneName();
+
     }
 }
